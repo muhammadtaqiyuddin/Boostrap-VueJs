c8451b7ae21f7ae23b452c1a714e9d9cea39dd36
Muhammad Taqiyuddin <mtaqiyuddin101@gmail.com>
2023-06-07 21:29:17 +0700

    Bootstrap & Vue Js

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Button from './Button.vue';
+</script>
+<template>
+  <div class="container py-5">
+    <div class="row flex-lg-row-reverse align-items-center d-flex g-5 justify-content-between py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_sk5h1kfn.json" background="transparent" speed="1"
+                        style="width: 100%; height: auto;" loop autoplay></lottie-player>
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">
+          Responsive left <br />
+          Aligned hero with image
+        </h1>
+        <p class="lead">
+          Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and
+          powerful JavaScript plugins.
+        </p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <Button text="Primary" class="btn-primary btn-lg px-4 me-md-2" />
+          <Button text="Default" class="btn-outline-secondary btn-lg px-4" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="navbar navbar-expand-lg border">
+    <div class="container d-flex">
+      <a class="navbar-brand" href="#">
+        <img src="../assets/logo.png" alt="" width="180" height="100" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Dropdown
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
